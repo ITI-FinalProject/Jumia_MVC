30dd30d7d4b29db5a06b3b012dc34b22c2043ce8
Nada Hamoda <eng.nada.hamoda@gmail.com>
2022-04-24 01:50:19 +0200

    contact us validation

--- Jumia_MVC/Models/ContactUs.cs
+++ Jumia_MVC/Models/ContactUs.cs
@@ -4,14 +4,15 @@ namespace Jumia_MVC.Models
 {
     public class ContactUs
     {
-        
-            [Required]
-            [StringLength(20, MinimumLength = 5)]
-            public string Name { get; set; }
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-            [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Name")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Name Should be min 5 and max 20 length")] 
+           public string Name { get; set; }
+
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide your Email")]
+            [EmailAddress (ErrorMessage = "Ex...@cdf.com")]
+        public string Email { get; set; }
+
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Subject")]
             public string Subject { get; set; }
             [Required]
             public string Message { get; set; }
