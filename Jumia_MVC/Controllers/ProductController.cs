c7658b5659588cda4598878da4b03f4da214817c
Shoukry Mohammed <shoukrymm86@gmail.com>
2022-04-23 21:09:52 +0200

    Search & Details

--- Jumia_MVC/Controllers/ProductController.cs
+++ Jumia_MVC/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using FinalProject.MVC.Data.services.Products;
 using FinalProject.MVC.Data.ViewModel;
+using Jumia_MVC.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -49,10 +51,30 @@ namespace Jumia_MVC.Controllers
         [HttpGet]
         public async Task<IActionResult> Details(int id)
         {
+
             if (id == null) return View("NotFound");
            var res=await _productsService.GetProductByIdAsync(id);
             if(res==null) return View("NotFound");
             return View(res);
+        }
+
+
+        //Filter
+        [AllowAnonymous]
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allProducts = await _productsService.GellAll(n => n.Category);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResultNew = allProducts.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+
+                //var filteredResultNew = allProducts.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+                return View("Index", filteredResultNew);
+            }
+
+            return View("Index", allProducts);
         }
 
     }
