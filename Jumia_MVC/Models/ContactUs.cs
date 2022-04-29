71c12974366e682b490223165e56956a44c270f0
Azkar <https://github.com/nermien990/Azkar>
2022-04-29 23:45:28 +0200

    localization in all views

--- Jumia_MVC/Models/ContactUs.cs
+++ Jumia_MVC/Models/ContactUs.cs
@@ -5,16 +5,16 @@ namespace Jumia_MVC.Models
     public class ContactUs
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Name")]
-        [StringLength(20, MinimumLength = 4, ErrorMessage = "Name Should be min 5 and max 20 length")] 
-           public string Name { get; set; }
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Name Should be min 5 and max 20 length")]
+        public string Name { get; set; }
 
-            [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide your Email")]
-            [EmailAddress (ErrorMessage = "Ex...@cdf.com")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide your Email")]
+        [EmailAddress(ErrorMessage = "Ex...@cdf.com")]
         public string Email { get; set; }
 
-            [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Subject")]
-            public string Subject { get; set; }
-            [Required]
-            public string Message { get; set; }
-        }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Subject")]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
     }
+}
